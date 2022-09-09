6e4a24dc21d552c3bc5da65330a5a2afaa238ab9
Andrey <70666153+andreu6454@users.noreply.github.com>
2022-09-10 01:41:49 +0300

    Update Count the photos!.js

--- Count the photos!.js
+++ Count the photos!.js
@@ -1,13 +1,10 @@
 function countPhotos(road) {
-    let count = 0, camerasCount = 0, LtoRCars = 0, RtoLCars = 0;
+    let count = 0, camerasCount = 0, LtoRCars = 0;
     road.split("").forEach(el => {
         if (el === ">") {
             LtoRCars++
-        } else if (el === "<") {
-            RtoLCars++
-            if (camerasCount !== 0) {
-                count += camerasCount
-            }
+        } else if (el === "<" && camerasCount !== 0) {
+            count += camerasCount
         } else {
             camerasCount++;
             count += LtoRCars
