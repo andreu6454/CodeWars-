e8c54d4adca4bc8c6fa69b00c92a471d6509bacc
Andrey <70666153+andreu6454@users.noreply.github.com>
2022-09-10 01:59:18 +0300

    Create Counting Change Combinations.js

---
+++ Counting Change Combinations.js
@@ -0,0 +1,11 @@
+var countChange = function (money, coins) {
+    if (money < 0 || coins.length === 0) {
+        return 0
+    }
+    if (money === 0) {
+        return 1
+    }
+    return (
+        countChange(money - coins[0], coins) + countChange(money, coins.slice(1))
+    )
+}
