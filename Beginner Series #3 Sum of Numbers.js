bbe516cc35ca62f374eaf70e64ea67380be50fc9
Andrey <70666153+andreu6454@users.noreply.github.com>
2022-12-07 20:49:04 +0300

    Create Beginner Series #3 Sum of Numbers.js

---
+++ Beginner Series #3 Sum of Numbers.js
@@ -0,0 +1,14 @@
+function getSum( a,b ){
+    if(a === b) return a
+    let sum = 0
+    if(a < b) {
+        for (let i = a; i <= b; i++){
+            sum += i
+        }
+    } else {
+        for (let i = b; i <= a; i++){
+            sum += i
+        }
+    }
+    return sum
+}
